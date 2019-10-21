77b5892a293c7a59fb1d5f7d85ad0c20d8359d7c
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-10-21 18:14:36 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<HTML>
+    <HEAD>
+        <STYLE>
+            BODY
+            {
+                TEXT-ALIGN : CENTER;
+                BACKGROUND-COLOR : LIGHTBLUE;
+                DISPLAY : INLINE;
+            }
+            .signup
+            {
+                TEXT-ALIGN : CENTER;
+                BACKGROUND-COLOR : POWDERBLUE;
+                POSITION : ABSOLUTE;
+                WIDTH : 30%;
+                HEIGHT : 50%;
+                TOP : 20%;
+                LEFT : 40%;
+            }
+            FORM
+            {
+                POSITION : ABSOLUTE;
+            }
+        </STYLE>
+    </HEAD>
+    <BODY>
+        <div CLASS = "signup">
+            <FORM name = "index.php" method = "GET">
+                login:<BR> <input type = "text" name = "login"><BR>
+                password<BR> <input type = "Password" name = "passwd">
+            </FORM>
+        </div>
+    </BODY>
+</HTML>
+<?php
+?>
