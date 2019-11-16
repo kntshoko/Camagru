52e41228f62b26f4f62a216320b735c62021139d
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-11-16 18:53:03 +0200

    CAMAGRU

--- gallary.php
+++ gallary.php
@@ -1,11 +1,55 @@
+
+<?php
+if(isset($_POST['submit'])){
+   // session_start();
+   // include_once ('config/setup.php');
+    //include_once ('config/database.php');
+    $name = $_SESSION['login'];
+    $file = $_FILES['file'];
+    $filename = $_FILES['file']['name'];
+    $fileTemp = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];  
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+     $fileExt = explode('.', $filename);
+     $fileActExt = strtolower(end($fileExt));
+     $allowed = array('jpg','jpeg','png','gif');
+     if (in_array($fileActExt, $allowed)){
+        if($fileError === 0){
+             if($fileSize < 10000000){
+                $newname = uniqid('', true).".".$fileActExt;
+                $filedest = 'uploads/'.$newname;
+                echo $file;
+     /*           try{
+                $sql = "INSERT INTO `camagru`.`gallery` (`name`,comments, images) VALUES (?,?,?)";
+                var_dump($sql);
+                $stmt = $conn->prepare($sql);
+                
+                $stmt->execute([$name,'', $newname]);
+                 move_uploaded_file($fileTemp, $filedest);
+                 header("location: home.php");
+                }catch(PDOException $e){
+                    echo $e->getMessage();
+                }*/
+             }else{
+                 echo "FILE IS TO LAREG";
+             }
+         }else{
+             echo "Error uploading";
+         }
+     }else{
+         echo "You uploaded wrong file type";
+    }
+}
+?>
 <html>
     <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
             body{
                 font-family: Arial, Helvetica, sans-serif;
             }
             form{
-                background-color: #333;
                 text-align : center;
             }
             .cll {
@@ -47,7 +91,7 @@
                 border : 1px solid #ccc;
                 box-sizing : border-box;
             }
-            input[type=submit]{
+            input[type=submit], input[type=file]{
                 width : 30%;
                 padding : 10px 18px;
                 margin : 8px 0;
@@ -75,12 +119,21 @@
             height : 60%;
             width : 40%;
         }
+        .foot label  
+            {
+            font-size: 16px;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            }
         .foot{
             position :absolute;
-            top : 80%;
+            top : 78%;
             height : 20%;
-            width : 60%;
-            padding :5%;
+            width : 78%;
+            padding : 0%;
+            background-color: #333;
         }
         h2{
             color: white; 
@@ -89,29 +142,47 @@
         .main
         {
             position :absolute;
-            top: 20%;
-            width: 60%;
-            padding : 5%;
-            height : 50%;
+            width: 78%;
+            padding : 0%;
+            height : 65%;
             background-color: #333;
+        }
+        .left{
+            position :absolute;
+            left : 80%;
+            height : 86%;
+            width : 20%;
+            padding : 00%;
+            background-color: #333;
+        }
+        .bd{
+            position :absolute;
+            width: 99%;
+            height : 95%;
+            top : 5%;
+            display : inline-block;
         }
         </style>
     </head>
     <body>
         <div class = "cll">
             <a href="index.php">Go To Welcome</a>
-        </div>      
-        <h1 align = center>
-            CAMAGRU
-        </h1>
-        <div class = "main">
+        </div>         
+        <div class = "bd">     
+            <h1 align = center>
+                CAMAGRU
+            </h1>
+                <div class = "main">
+                </div>
+                <div class = "foot">
+                    <form action="upload.php" method ="post" enctype = "multipart/form-data">
+                       <label for="filetoupload">Select image to upload:</label> 
+                        <input type="file" name = "filetoupload" id = "filetoupload>">
+                        <input type="submit" value = "upload image">
+                    </form>    
+                </div>  
+                <div class="left">
+                </div> 
         </div>
-            <div class = "foot">
-                <form action="upload.php" method ="post" enctype = "multipart/form-data">
-                    Select image to upload:
-                    <input type="file" name = "filetoupload" id = "filetoupload>">
-                    <input type="submit" value = "upload image">
-                </form>    
-            </div>      
     </body>
 </html>
