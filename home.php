dd6c4ab3cc5e2a31c05c26759c62d5bbbb1aed5a
Khotso NTSHOKO <kntshoko@c4r5s4.wethinkcode.co.za>
2019-11-19 18:34:49 +0200

    CAMAGRU

--- home.php
+++ home.php
@@ -2,13 +2,39 @@
 <head>
     <style>
         body{
-            background-color: rgb(250,200,150) ;
+            /* background-color: rgb(250,200,150) ; */
         }
     </style>
 </head>
 <body>
-    <h1 align = "center">
-        CAMAGRU
-    </h1>
+<?php
+    echo "yyyyy";
+                //require "setup.php";
+                require_once ("setup.php");
+                require_once("config.php");
+
+                try
+                {
+                    $sql = $conn->prepare("SELECT * FROM `gallery` "); 
+                    
+                    $sql->execute();
+                    // $result = $sql->fetch(PDO::FETCH_ASSOC)
+                    var_dump($sql);
+                while ($row = $sql->fetch(PDO::FETCH_ASSOC))
+                {
+                    echo '<A href= "#">
+                    <DIV class="gallery-image" style= "background-image: url(uploads/'.$row['image_name'].');" width = 300 height = 300 ></DIV></A>
+                    <h2>'.$row['image_uploader_name'].'</h2>
+                    <p> '.$row['image_caption'].'<p>
+                    ';    
+                }
+                }   
+                catch(PDOExceptipn $e)
+                {
+                    echo $e->getMessage();
+                }
+  
+                
+            ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-    //require "setup.php";
-?>
+
 <html>
     <head>
         <title>Document</title>
